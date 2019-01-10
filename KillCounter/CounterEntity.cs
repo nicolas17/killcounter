8c2191f2b3fc652ba14ae98f3ec6976aaace9798
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2019-01-10 19:05:53 -0300

    Initial code showing the death counter

---
+++ KillCounter/CounterEntity.cs
@@ -0,0 +1,36 @@
+// Copyright (c) 2019 Nicolás Alvarez <nicolas.alvarez@gmail.com>
+// Licensed under the MIT license;
+// see LICENSE.txt for details.
+
+using System;
+using System.Diagnostics;
+using Monocle;
+using Celeste;
+using Microsoft.Xna.Framework;
+
+namespace KillCounter
+{
+    [Tracked(false)]
+    class CounterEntity: Entity
+    {
+        private Level level;
+        private DeathsCounter counter;
+
+        public CounterEntity(Level level)
+        {
+            Position = new Vector2(0f, 30f);
+            Depth = -100;
+            Tag = (Tags.HUD | Tags.Global);
+
+            base.Add(this.counter = new DeathsCounter(AreaMode.Normal, false, 42));
+
+            this.level = level;
+        }
+        public override void Update()
+        {
+            base.Update();
+            AreaKey area = level.Session.Area;
+            this.counter.Amount = SaveData.Instance.Areas_Safe[area.ID].Modes[(int)area.Mode].Deaths;
+        }
+    }
+}
--- KillCounter/KillCounterModule.cs
+++ KillCounter/KillCounterModule.cs
@@ -3,10 +3,47 @@
 // see LICENSE.txt for details.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Celeste;
+using Celeste.Mod;
 
 namespace KillCounter
 {
-    public class KillCounterModule
+    public class KillCounterModule : EverestModule
     {
+        public override Type SettingsType => null;
+
+        public override void Load()
+        {
+            Debug.WriteLine("Load called");
+            On.Celeste.Level.LoadLevel += OnLoadLevel;
+            Everest.Events.Level.OnExit += OnExit;
+        }
+
+        private void OnExit(Level level, LevelExit exit, LevelExit.Mode mode, Session session, HiresSnow snow)
+        {
+            Debug.WriteLine($"OnExit({level}, {exit}, {mode}, {session}, {snow})");
+        }
+
+        private void OnLoadLevel(On.Celeste.Level.orig_LoadLevel orig, Celeste.Level level, Celeste.Player.IntroTypes playerIntro, bool isFromLoader)
+        {
+            orig(level, playerIntro, isFromLoader);
+            Debug.WriteLine($"OnLoadLevel called, intro type {playerIntro}, isFromLoader {isFromLoader}");
+            if (isFromLoader) {
+                List<Monocle.Entity> e = level.Tracker.GetEntities<CounterEntity>();
+                if (e.Count == 1) {
+                    Debug.WriteLine("counter already exists");
+                } else {
+                    Debug.WriteLine("Creating counter entity");
+                    level.Add(new CounterEntity(level));
+                }
+            }
+        }
+
+        public override void Unload()
+        {
+            Debug.WriteLine("Unload called");
+        }
     }
 }
