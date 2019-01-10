9d620aa8503593cb6c963e08bf48a531767bb443
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2019-01-09 21:06:47 -0300

    Add project files.

---
+++ KillCounter/Class1.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 Nicolás Alvarez <nicolas.alvarez@gmail.com>
+// Licensed under the MIT license;
+// see LICENSE.txt for details.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillCounter
+{
+    public class Class1
+    {
+    }
+}
