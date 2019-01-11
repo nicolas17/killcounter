564354348ca4a9801a303ac669daeda3e1598a3b
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2019-01-10 22:14:00 -0300

    Set version information properly

--- KillCounter/Properties/AssemblyInfo.cs
+++ KillCounter/Properties/AssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KillCounter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Celeste mod to show a death counter on screen")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nicolás Alvarez")]
 [assembly: AssemblyProduct("KillCounter")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 Nicolás Alvarez")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
